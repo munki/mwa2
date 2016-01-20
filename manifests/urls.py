dffda5590a5a8d875e8ff9ef75dc5fe475683905
Greg Neagle <gregneagle@mac.com>
2016-01-20 15:55:01 -0800

    Login form is now a Django modal. Fixes #5. Fixes #12.

--- manifests/urls.py
+++ manifests/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 import manifests.views
 
 urlpatterns = [
-    url(r'^$', manifests.views.index),
+    url(r'^$', manifests.views.index, name='manifests'),
     url(r'^__get_manifest_list_status$', manifests.views.status),
     url(r'^(?P<manifest_path>^.*$)', manifests.views.detail)
 ]
