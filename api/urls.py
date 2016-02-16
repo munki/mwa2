22deac7353cd34f924bb34762755f439f8434762
Greg Neagle <gregneagle@mac.com>
2016-02-16 12:21:33 -0800

    Don't require trailing slash for API collection operations. (IOW http://server/api/manifests is now a valid request)

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 import api.views
 
 urlpatterns = [
+    url(r'^(?P<kind>manifests$)', api.views.api),
+    url(r'^(?P<kind>pkgsinfo$)', api.views.api),
     url(r'^(?P<kind>manifests)/(?P<filepath>.*$)', api.views.api),
     url(r'^(?P<kind>pkgsinfo)/(?P<filepath>.*$)', api.views.api)
 ]
